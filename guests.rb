6ffff63b01861fd009486000f55a59fd8e2ed516
Rhys Bowen Harries <rbharries@gmail.com>
2019-04-15 09:09:47 +0100

    everything but the woo

--- guests.rb
+++ guests.rb
@@ -1,6 +1,6 @@
 class Guests
 
-attr_reader :name, :credit, :fave_song
+attr_reader :name, :credit, :fave_song, :entrance_fee, :beer_price, :champagne
 
 
   def initialize(name, credit, fave_song)
@@ -8,15 +8,42 @@ attr_reader :name, :credit, :fave_song
     @credit = credit
     @fave_song = fave_song
 
+    @entrance_fee = 5
+    @beer_price = 4
+    @champagne = 20
   end
 
 
+  def woo
+    return "woo"
+  end
 
+  # def supply_fish(credit)
+  #   return @fishes.pop
+  # end
 
+  def supply_entrance_fee
+    deduction = @credit -= @entrance_fee
+    return @entrance_fee
+  end
 
+  # def supply_product(product)
+  #   @credit -= product
+  #   p product
+  # end
+  def charge_beer
+    deduction = @credit -= @beer_price
+    return @beer_price
+  end
 
-
-
+  def charge_champagne
+    if @champagne < @credit
+      deduction = @credit -= @champagne
+      return @champagne
+    else
+      return 0
+    end
+  end
 
 
 
--- room.rb
+++ room.rb
@@ -1,44 +1,56 @@
 class Room
 
-attr_accessor :capacity, :number_of_guests, :song_array
+attr_accessor :capacity, :number_of_guests, :song_array, :entry_fee, :spend_count
 
-  def initialize(capacity, number_of_guests)
+  def initialize(capacity, guest_list)
     @capacity = capacity
-    @number_of_guests = number_of_guests
+    @guest_list = guest_list
+    # @number_of_guests = guest_list.count
     @song_array = []
+    @spend_count = 0
 
+  end
 
+  def guest_counter
+    return @guest_list.count
   end
 
   def add_song(song)
     @song_array += [(song)]
-    p @song_array
   end
 
   def remove_song(song)
     @song_array -= [(song)]
   end
 
-  def add_guest(number)
-    if (capacity - number_of_guests) > number
-      @number_of_guests += number
+  def add_guest(guests)
+    if (capacity != @guest_list.count)
+      @guest_list.push(guests)
+      p @guest_list
     else
       return "Not Enough Room"
     end
   end
 
-  def remove_guest(number)
-    @number_of_guests -= number
-    if @number_of_guests < 0
-      @number_of_guests = 0
-    end   
+  def remove_guest(name)
+    @guest_list.delete(name)
+    # if @number_of_guests < 0
+    #   @number_of_guests = 0
   end
 
 
 
+  # def take_fish(credit)_from_river(customer)(river)
+  #   fish = river.supply_fish()
+  #   @belly << fish if !fish.nil?
+  # end
 
+  def charge_entrance_fee(guest)
+    @spend_count += guest.supply_entrance_fee
+  end
 
-
-
+  def charge_champagne(guest)
+    @spend_count += guest.charge_champagne
+  end  
 
 end
